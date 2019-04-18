cc2faa537942b9dfaa07f229aa9f06643df560a0
iroykaufman <49757154+iroykaufman@users.noreply.github.com>
2019-04-18 17:57:56 +0300

    Delete main.py

--- main.py
+++
@@ -1,9 +0,0 @@
-import cv2
-import os
-from PIL import Image
-import numpy as np
-from createfolder import createFolder
-from train import trainprodict
-from colectdata import colectdata
-colectdata()
-trainprodict()
